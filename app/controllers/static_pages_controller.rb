759f953ed5822fc759a07ea3beeaf6f819c77ded
Dianaviento <dianaviento@gmail.com>
2014-09-29 13:44:14 +0000

    Chapter 10

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-      @micropost = current_user.microposts.build if signed_in?
+      if signed_in?
+      @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
+ 
 
   def about
   end
@@ -13,4 +15,4 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
-
+end
